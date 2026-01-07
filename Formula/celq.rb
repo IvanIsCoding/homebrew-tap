2a5b065681349146e4abf179f9f8426adca2830c
Ivan Carvalho <8753214+IvanIsCoding@users.noreply.github.com>
2026-01-06 23:57:27 -0500

    Update Celq to version 0.1.1

--- Formula/celq.rb
+++ Formula/celq.rb
@@ -1,8 +1,8 @@
 class Celq < Formula
   desc "A Common Expression Language (CEL) CLI Tool"
   homepage "https://github.com/IvanIsCoding/celq"
-  url "https://github.com/IvanIsCoding/celq/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "997c716b902b4dfbad9adad765511b93253e3e66ad8abf3e70214638314b7cd8"
+  url "https://github.com/IvanIsCoding/celq/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT OR Apache-2.0"
 
   depends_on "rust" => :build
